3ab6e0904f2c9dcd1db3194eb6ca6e5febc5e431
willy-vvu <willy.vvu@gmail.com>
2014-03-25 11:55:37 -0400

    Added Display class and bare-bones main method

---
+++ src/Display.java
@@ -0,0 +1,19 @@
+import javax.swing.JFrame;
+
+/**
+ * Display.java
+ * 
+ * Written Mar 25, 2014
+ * 
+ * @author Ted Beiber, William Wu, and Rohan Kadambi
+ * 
+ */
+public class Display {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(800, 800);
+		frame.setLocation(10, 10);
+		frame.setVisible(true);
+	}
+}
