ec2652cd954831d6069ce4870e67bcda7329c6c2
14kadambir <14kadambir@HS-280W-Z210-23.ab-hayward.net>
2014-03-26 11:02:10 -0400

    Added comment in Player.java and Grid.java

---
+++ src/Grid.java
@@ -0,0 +1,22 @@
+/*
+ * Grid (3x3/ rectangle) 
+ * Constructor: (int width, int height)
+ * boolean isValid(int x, int y)
+ * 		(0,0) is top left
+ * PuzzlePiece setCell(int x, int y, PuzzlePiece piece)
+ * 		returns old data
+ * 		if not valid return null
+ * PuzzlePiece getCell(int x, int y)
+ * 		return null if not valid
+ * void clear()
+ * int getHeight()
+ * int getWidth()
+ * boolean isOccupied(int x, int y)
+ * boolean isFull()
+ * 		returns true if each location in the grid contains a PuzzlePiece
+ * boolean isEmpty()
+
+ */
+public class Grid {
+
+}
--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,20 @@
-
+/*
+ * variables
+ * 	grid
+ * 	pieces in bank
+ * void solve()
+ * Constructor: (Grid grid, PuzzlePiece[] pieces)
+ * Grid getGrid()
+ * PuzzlePiece[] get$Bank()
+ * boolean canPlace(int x, int y, PuzzlePiece piece)
+ *		returns false if invalid location or does not fit
+ * 		checks left, top, bottom, right and see if parts align
+ * 		returns true if specified location is valid for piece
+ * boolean place(int x, int y, PuzzlePiece piece)
+ * 		returns true only if the piece was places, false if it couldn’t be
+ * boolean remove(int x, int y)
+ * 		returns true if a piece was removed
+ */
 public class Player {
 
 }
