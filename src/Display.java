ca480e283f40412260d1ebc5fef2f4d96e937719
willy-vvu <willy.vvu@gmail.com>
2014-03-26 11:42:33 -0400

    Added bare bones drawing component

--- src/Display.java
+++ src/Display.java
@@ -1,3 +1,14 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
+import java.util.Date;
+
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 
 /**
@@ -14,6 +25,76 @@ public class Display {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(800, 800);
 		frame.setLocation(10, 10);
+		frame.add(new PuzzleDrawingComponent());
 		frame.setVisible(true);
+	}
+}
+
+/**
+ * A component to draw the puzzle and its pieces.
+ * 
+ */
+class PuzzleDrawingComponent extends JComponent {
+	private long lastTick = 0;
+
+	public PuzzleDrawingComponent() {
+
+		this.addMouseListener(new MouseListener() {
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent e) {
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+			}
+
+		});
+		this.addMouseMotionListener(new MouseMotionListener() {
+
+			@Override
+			public void mouseDragged(MouseEvent e) {
+			}
+
+			@Override
+			public void mouseMoved(MouseEvent e) {
+			}
+		});
+		// Listen for mouse scrolling
+		this.addMouseWheelListener(new MouseWheelListener() {
+			@Override
+			public void mouseWheelMoved(MouseWheelEvent e) {
+			}
+		});
+	}
+
+	/**
+	 * @param graphics
+	 *            the graphics context
+	 * 
+	 */
+	public void paintComponent(Graphics graphics) {
+		// Extract Graphics2D
+		Graphics2D g = (Graphics2D) graphics;
+		// adds some anti-Aliasing
+		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+		// Draw background
+
+		long currentTime = new Date().getTime();
+		double timeElapsed = (lastTick == 0 ? 0 : (currentTime - lastTick)) * 0.001;
+		lastTick = currentTime;
 	}
 }
