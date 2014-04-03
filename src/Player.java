d252893e07f5b629b8bcf26c561e9a3100b101de
notayankeesfan <rohan.kadambi@gmail.com>
2014-04-03 19:16:03 -0400

    Update Player.java
    Fixed Player Class, wrote Solve().
    Issues when running display, ArrayIndexOutOfBounds thrown. get$Bank[] changes size while repaint tries to draw it I believe.

--- src/Player.java
+++ src/Player.java
@@ -22,13 +22,44 @@ public class Player {
 	public Player(Grid grid, PuzzlePiece[] pieces) {
 		this.grid = grid;
 		this.pieces = pieces;
-		for(int i = 0; i<pieces.length; i++)
+		for (int i = 0; i < pieces.length; i++)
 			bank.add(pieces[i]);
 	}
 
-	//Solve doesn't work, yet
-	public void solve() {
+	public boolean solve() {
+		if (grid.isFull())
+			return true;
+		for (int i = bank.size() - 1; i > -1; i--) {
+			for (int j = 0; j < 4; j++) {
+				bank.get(i).rotate();
+				if (place(nextSpot()[0], nextSpot()[1], bank.get(i))) {
+					solve();
+					if (grid.isFull())
+						return true;
+					bank.add(i, grid.getCell(lastPlaced()[0], lastPlaced()[1]));
+					grid.setCell(lastPlaced()[0], lastPlaced()[1], null);
+				}
+			}
 
+		}
+		return false;
+
+	}
+
+	public int[] nextSpot() {
+		for (int i = 0; i < grid.getHeight(); i++)
+			for (int j = 0; j < grid.getWidth(); j++)
+				if (grid.getCell(i, j) == null)
+					return new int[] { i, j };
+		return new int[] { 0, 0 };
+	}
+
+	public int[] lastPlaced() {
+		for (int i = grid.getHeight() - 1; i > -1; i--)
+			for (int j = grid.getWidth() - 1; j > -1; j--)
+				if (grid.getCell(i, j) != null)
+					return new int[] { i, j };
+		return new int[] { grid.getHeight() - 1, grid.getWidth() - 1 };
 	}
 
 	public Grid getGrid() {
@@ -37,53 +68,53 @@ public class Player {
 
 	public PuzzlePiece[] get$Bank() {
 		PuzzlePiece[] temp = new PuzzlePiece[bank.size()];
-		for(int i=0;i<bank.size();i++)
+		for (int i = 0; i < bank.size(); i++)
 			temp[i] = bank.get(i);
 		return temp;
 	}
 
 	public boolean canPlace(int x, int y, PuzzlePiece piece) {
-		if(grid.isValid(x, y))
-		{
-			//check north
+		if (grid.isValid(x, y)) {
+			// check north
 			boolean north;
-			if(adjacentSide(x,y,PuzzlePiece.NORTH) == 0)
+			if (adjacentSide(x, y, PuzzlePiece.NORTH) == 0)
 				north = true;
 			else
-				north = piece.getSide(PuzzlePiece.NORTH) + adjacentSide(x,y,PuzzlePiece.NORTH) ==0;
-			//check east
+				north = piece.getSide(PuzzlePiece.NORTH)
+						+ adjacentSide(x, y, PuzzlePiece.NORTH) == 0;
+			// check east
 			boolean east;
-			if(adjacentSide(x,y,PuzzlePiece.EAST) == 0)
+			if (adjacentSide(x, y, PuzzlePiece.EAST) == 0)
 				east = true;
 			else
-				east = piece.getSide(PuzzlePiece.EAST) + adjacentSide(x,y,PuzzlePiece.EAST) ==0;
-			//check west
+				east = piece.getSide(PuzzlePiece.EAST)
+						+ adjacentSide(x, y, PuzzlePiece.EAST) == 0;
+			// check west
 			boolean south;
-			if(adjacentSide(x,y,PuzzlePiece.SOUTH) == 0)
+			if (adjacentSide(x, y, PuzzlePiece.SOUTH) == 0)
 				south = true;
 			else
-				south = piece.getSide(PuzzlePiece.SOUTH) + adjacentSide(x,y,PuzzlePiece.SOUTH) ==0;
-			//check south
+				south = piece.getSide(PuzzlePiece.SOUTH)
+						+ adjacentSide(x, y, PuzzlePiece.SOUTH) == 0;
+			// check south
 			boolean west;
-			if(adjacentSide(x,y,PuzzlePiece.WEST) == 0)
+			if (adjacentSide(x, y, PuzzlePiece.WEST) == 0)
 				west = true;
 			else
-				west = piece.getSide(PuzzlePiece.WEST) + adjacentSide(x,y,PuzzlePiece.WEST) ==0;
-			return north&&east&&south&&west;
+				west = piece.getSide(PuzzlePiece.WEST)
+						+ adjacentSide(x, y, PuzzlePiece.WEST) == 0;
+			return north && east && south && west;
 		}
 		return false;
 	}
 
 	public boolean place(int x, int y, PuzzlePiece piece) {
-
-		if(canPlace(x,y,piece))
-		{
+		if (grid.isValid(x, y) && canPlace(x, y, piece)) {
 			grid.setCell(x, y, piece);
 			bank.remove(piece);
 			return true;
 		}
 		return false;
-
 	}
 
 	public boolean remove(int x, int y) {
@@ -92,82 +123,77 @@ public class Player {
 		return true;
 	}
 
-
-	public int adjacentSide(int x, int y, int direction)
-	{
-		if(direction == PuzzlePiece.NORTH)
-		{
-			if(grid.isValid(x,y-1) && grid.isOccupied(x,y-1))
-				return grid.getCell(x, y-1).getSide(PuzzlePiece.SOUTH);
+	public int adjacentSide(int x, int y, int direction) {
+		if (direction == PuzzlePiece.NORTH) {
+			if (grid.isOccupied(x, y - 1))
+				return grid.getCell(x, y - 1).getSide(PuzzlePiece.SOUTH);
 			return 0;
 		}
-		if(direction == PuzzlePiece.EAST)
-		{
-			if(grid.isValid(x+1, y) && grid.isOccupied(x+1, y))
-				return grid.getCell(x+1, y).getSide(PuzzlePiece.WEST);
+		if (direction == PuzzlePiece.EAST) {
+			if (grid.isOccupied(x + 1, y))
+				return grid.getCell(x + 1, y).getSide(PuzzlePiece.WEST);
 			return 0;
 		}
-		if(direction == PuzzlePiece.SOUTH)
-		{
-			if(grid.isValid(x, y+1) && grid.isOccupied(x,y+1))
-				return grid.getCell(x, y+1).getSide(PuzzlePiece.NORTH);
+		if (direction == PuzzlePiece.SOUTH) {
+			if (grid.isOccupied(x, y + 1))
+				return grid.getCell(x, y + 1).getSide(PuzzlePiece.NORTH);
 			return 0;
 		}
-		if(direction == PuzzlePiece.WEST)
-		{
-			if(grid.isValid(x-1, y) && grid.isOccupied(x-1, y))
-				return grid.getCell(x-1, y).getSide(PuzzlePiece.EAST);
+		if (direction == PuzzlePiece.WEST) {
+			if (grid.isOccupied(x - 1, y))
+				return grid.getCell(x - 1, y).getSide(PuzzlePiece.EAST);
 			return 0;
 		}
 		return 0;
 
 	}
+
 	private Grid grid;
 	private PuzzlePiece[] pieces;
 	private ArrayList<PuzzlePiece> bank = new ArrayList<PuzzlePiece>();
 
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		PuzzlePiece[] puzzlePieces = {
 				new PuzzlePiece(PuzzlePiece.CLUBS_OUT, PuzzlePiece.HEARTS_OUT,
 						PuzzlePiece.DIAMONDS_IN, PuzzlePiece.CLUBS_IN),
-						new PuzzlePiece(PuzzlePiece.SPADES_OUT,
-								PuzzlePiece.DIAMONDS_OUT, PuzzlePiece.SPADES_IN,
-								PuzzlePiece.HEARTS_IN),
-								new PuzzlePiece(PuzzlePiece.HEARTS_OUT, PuzzlePiece.SPADES_OUT,
-										PuzzlePiece.SPADES_IN, PuzzlePiece.CLUBS_IN),
-										new PuzzlePiece(PuzzlePiece.HEARTS_OUT,
-												PuzzlePiece.DIAMONDS_OUT, PuzzlePiece.CLUBS_IN,
-												PuzzlePiece.CLUBS_IN),
-												new PuzzlePiece(PuzzlePiece.SPADES_OUT, PuzzlePiece.SPADES_OUT,
-														PuzzlePiece.HEARTS_IN, PuzzlePiece.CLUBS_IN),
-														new PuzzlePiece(PuzzlePiece.HEARTS_OUT,
-																PuzzlePiece.DIAMONDS_OUT, PuzzlePiece.DIAMONDS_IN,
-																PuzzlePiece.HEARTS_IN),
-																new PuzzlePiece(PuzzlePiece.SPADES_OUT,
-																		PuzzlePiece.DIAMONDS_OUT, PuzzlePiece.HEARTS_IN,
-																		PuzzlePiece.DIAMONDS_IN),
-																		new PuzzlePiece(PuzzlePiece.CLUBS_OUT, PuzzlePiece.HEARTS_OUT,
-																				PuzzlePiece.SPADES_IN, PuzzlePiece.HEARTS_IN),
-																				new PuzzlePiece(PuzzlePiece.DIAMONDS_OUT,
-																						PuzzlePiece.CLUBS_OUT, PuzzlePiece.CLUBS_IN,
-																						PuzzlePiece.DIAMONDS_IN) };
+				new PuzzlePiece(PuzzlePiece.SPADES_OUT,
+						PuzzlePiece.DIAMONDS_OUT, PuzzlePiece.SPADES_IN,
+						PuzzlePiece.HEARTS_IN),
+				new PuzzlePiece(PuzzlePiece.HEARTS_OUT, PuzzlePiece.SPADES_OUT,
+						PuzzlePiece.SPADES_IN, PuzzlePiece.CLUBS_IN),
+				new PuzzlePiece(PuzzlePiece.HEARTS_OUT,
+						PuzzlePiece.DIAMONDS_OUT, PuzzlePiece.CLUBS_IN,
+						PuzzlePiece.CLUBS_IN),
+				new PuzzlePiece(PuzzlePiece.SPADES_OUT, PuzzlePiece.SPADES_OUT,
+						PuzzlePiece.HEARTS_IN, PuzzlePiece.CLUBS_IN),
+				new PuzzlePiece(PuzzlePiece.HEARTS_OUT,
+						PuzzlePiece.DIAMONDS_OUT, PuzzlePiece.DIAMONDS_IN,
+						PuzzlePiece.HEARTS_IN),
+				new PuzzlePiece(PuzzlePiece.SPADES_OUT,
+						PuzzlePiece.DIAMONDS_OUT, PuzzlePiece.HEARTS_IN,
+						PuzzlePiece.DIAMONDS_IN),
+				new PuzzlePiece(PuzzlePiece.CLUBS_OUT, PuzzlePiece.HEARTS_OUT,
+						PuzzlePiece.SPADES_IN, PuzzlePiece.HEARTS_IN),
+				new PuzzlePiece(PuzzlePiece.DIAMONDS_OUT,
+						PuzzlePiece.CLUBS_OUT, PuzzlePiece.CLUBS_IN,
+						PuzzlePiece.DIAMONDS_IN) };
 
-		Player  p= new Player(new Grid(3, 3), puzzlePieces);
-		//		p.get$Bank()[0].rotateTheWayOppositeOfTheOtherRotateMethod();
-		//		System.out.println(p.place(0, 1, p.get$Bank()[0]));
-		//		System.out.println(p.place(0, 0, p.get$Bank()[0]));
-		//		System.out.println(p.place(-1, -1, p.get$Bank()[0]));
-		//		System.out.println(p.place(1, 0, p.get$Bank()[0]));
-		//		int[] a = p.nextSpot();
-		//		int[] b = p.lastPlaced();
-		//		for(int i = 0; i<2; i ++)
-		//			System.out.println(a[i]);
-		//		for(int i = 0; i<2 ;i++)
-		//			System.out.println(b[i]);
-
-
+		Player p = new Player(new Grid(3, 3), puzzlePieces);
+		System.out.println(p.solve());
+		// for(int i =0; i<p.getGrid().getHeight(); i++)
+		// 	for(int j = 0; j<p.getGrid().getWidth(); j++)
+		// 		System.out.println(p.getGrid().getCell(j, i));
+		// p.get$Bank()[0].rotateTheWayOppositeOfTheOtherRotateMethod();
+		// System.out.println(p.place(0, 1, p.get$Bank()[0]));
+		// System.out.println(p.place(0, 0, p.get$Bank()[0]));
+		// System.out.println(p.place(-1, -1, p.get$Bank()[0]));
+		// System.out.println(p.place(1, 0, p.get$Bank()[0]));
+		// int[] a = p.nextSpot();
+		// int[] b = p.lastPlaced();
+		// for(int i = 0; i<2; i ++)
+		// System.out.println(a[i]);
+		// for(int i = 0; i<2 ;i++)
+		// System.out.println(b[i]);
 
 	}
 }
-
