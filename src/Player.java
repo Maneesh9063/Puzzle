83a94b109cff7fa9af36eb0692bb0907e8ee2ccc
notayankeesfan <rohan.kadambi@gmail.com>
2014-04-28 21:59:20 -0400

    Update Player.java
    Added Hunter's Comments

--- src/Player.java
+++ src/Player.java
@@ -1,26 +1,48 @@
 import java.util.ArrayList;
 
-
+/*
+ * PURPOSE: A class that creates a Grid object and applies standard puzzle rules,
+ * 			manipulates the data to simulate a puzzle game on the Grid instance.
+ * AUTHOR: Hunter John Larco, John Bieber
+ * CLASS: Player.java
+ */
 public class Player {
 
+	/*
+	 * PURPOSE: Constructor receiving the grid to play on and the pieces one may use.
+	 * RETURNS: <object Player>
+	 */
 	public Player(Grid grid, PuzzlePiece[] pieces){
 		this.grid = grid;
 		for(PuzzlePiece p : pieces)
 			bank.add(p);
 	}
 	
+	/*
+	 * PURPOSE: Clears the board
+	 * RETURNS: void
+	 */
 	public void clear(){
 		for(int x=0; x<grid.getWidth(); x++)
 			for(int y=0; y<grid.getHeight(); y++)
 				remove(x,y);
 	}
 	
+	/*
+	 * PURPOSE: Randomizes the rotations of the puzzle pieces
+	 * RETURNS: void
+	 */
 	public void randomize(){
 		for(PuzzlePiece p : bank)
 			for(int i=0; i<(int)(Math.random()*4); i++)
 					p.rotate();
 	}
 
+	/*
+	 * PURPOSE: Solves the puzzle and moves the pieces into their correct locations.
+	 * 			Recursively branches using backtracking until a correct solution is found.
+	 * RETURNS: boolean
+	 */
 	public boolean solve(){
 		// ensure that the board is cleared before solving
 		clear();
@@ -32,7 +54,7 @@ public class Player {
 		int y = index/grid.getWidth();
 		int x = index-index/grid.getWidth()*grid.getWidth();
 		for(int i=bank.size()-1; i>-1; i--){
-			for(int j=0; j<5; j++){
+			for(int j=0; j<4; j++){
 				if(place(x,y,bank.get(i))){
 					if(pSolve(index+1)) return true;
 					remove(x,y);
@@ -43,16 +65,29 @@ public class Player {
 		return false;
 	}
 
+	/*
+	 * PURPOSE: Returns the raw Grid instance that the pieces are organized and held in.
+	 * RETURNS: <object Grid>
+	 */
 	public Grid getGrid(){
 		return grid;
 	}
 
+	/*
+	 * PURPOSE: Returns all the pieces currently not fitted on the board
+	 * RETURNS: PuzzlePiece[]
+	 */
 	public PuzzlePiece[] get$Bank(){
 		PuzzlePiece[] out = new PuzzlePiece[bank.size()];
 		for(int i=0; i<out.length; i++) out[i] = bank.get(i);
 		return out;
 	}
 
+	/*
+	 * PURPOSE: Returns whether if the provided PuzzlePiece fill fit at the given location (x, y)
+	 * 			based on the surrounding pieces and location validity.
+	 * RETURNS: boolean
+	 */
 	public boolean canPlace(int x, int y, PuzzlePiece piece){
 		if(!grid.isValid(x, y) || grid.getCell(x, y) != null) return false;
 		if(grid.getCell(x, y-1) != null && grid.getCell(x, y-1).getSide(PuzzlePiece.SOUTH)+piece.getSide(PuzzlePiece.NORTH)!=0) return false;
@@ -62,6 +97,10 @@ public class Player {
 		return true;
 	}
 
+	/*
+	 * PURPOSE: Places a given piece at the given location (x, y)
+	 * RETURNS: boolean ~ whether the piece was actually placed, it will not be if it does not fit.
+	 */
 	public boolean place(int x, int y, PuzzlePiece piece){
 		if(canPlace(x,y,piece)){
 			bank.remove(piece);
@@ -71,6 +110,10 @@ public class Player {
 		return false;
 	}
 
+	/*
+	 * PURPOSE: Removes a piece from the board at the given location (x, y)
+	 * RETURNS: boolean ~ whether or not a piece could be removed
+	 */
 	public boolean remove(int x, int y){
 		return grid.getCell(x, y) != null && grid.isValid(x, y) ? bank.add(grid.setCell(x,y,null)) : false;
 	}
@@ -78,26 +121,8 @@ public class Player {
 	public String toString(){
 		return grid.toString();
 	}
-
-	public static void main(String[] args){
-		Grid grid = new Grid(3,3);
-		PuzzlePiece piece1 = new PuzzlePiece(PuzzlePiece.CLUBS_OUT,PuzzlePiece.HEARTS_OUT,PuzzlePiece.DIAMONDS_IN,PuzzlePiece.CLUBS_IN);
-		PuzzlePiece piece2 = new PuzzlePiece(PuzzlePiece.SPADES_OUT,PuzzlePiece.DIAMONDS_OUT,PuzzlePiece.SPADES_IN,PuzzlePiece.HEARTS_IN);
-		PuzzlePiece piece3 = new PuzzlePiece(PuzzlePiece.HEARTS_OUT,PuzzlePiece.SPADES_OUT,PuzzlePiece.SPADES_IN,PuzzlePiece.CLUBS_IN);
-		PuzzlePiece piece4 = new PuzzlePiece(PuzzlePiece.HEARTS_OUT,PuzzlePiece.DIAMONDS_OUT,PuzzlePiece.CLUBS_IN,PuzzlePiece.CLUBS_IN);
-		PuzzlePiece piece5 = new PuzzlePiece(PuzzlePiece.SPADES_OUT,PuzzlePiece.SPADES_OUT,PuzzlePiece.HEARTS_IN,PuzzlePiece.CLUBS_IN);
-		PuzzlePiece piece6 = new PuzzlePiece(PuzzlePiece.HEARTS_OUT,PuzzlePiece.DIAMONDS_OUT,PuzzlePiece.DIAMONDS_IN,PuzzlePiece.HEARTS_IN);
-		PuzzlePiece piece7 = new PuzzlePiece(PuzzlePiece.SPADES_OUT,PuzzlePiece.DIAMONDS_OUT,PuzzlePiece.HEARTS_IN,PuzzlePiece.DIAMONDS_IN);
-		PuzzlePiece piece8 = new PuzzlePiece(PuzzlePiece.CLUBS_OUT,PuzzlePiece.HEARTS_OUT,PuzzlePiece.SPADES_IN,PuzzlePiece.HEARTS_IN);
-		PuzzlePiece piece9 = new PuzzlePiece(PuzzlePiece.CLUBS_OUT,PuzzlePiece.CLUBS_IN,PuzzlePiece.DIAMONDS_IN,PuzzlePiece.DIAMONDS_OUT);
-		// These pieces are specialized to be the ones Mr. Marshall gave us.
-		PuzzlePiece[] bank2 = {piece1,piece2,piece3,piece4,piece5,piece6,piece7,piece8,piece9};
-		Player player = new Player(grid,bank2);
-
-		System.out.println(player.solve());
-		
-		System.out.println(player);
-	}
+	
+	// ---------- PRIVATE DATA ---------- \\
 
 	private Grid grid;
 	private ArrayList<PuzzlePiece> bank = new ArrayList<PuzzlePiece>();
