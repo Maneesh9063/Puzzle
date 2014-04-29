0351653388af5d64261e712d90fd0f45f1bbf342
notayankeesfan <rohan.kadambi@gmail.com>
2014-04-28 21:57:49 -0400

    Update Grid.java
    Added Hunter's comments

--- src/Grid.java
+++ src/Grid.java
@@ -1,14 +1,30 @@
-
+/*
+ * AUTHOR: Hunter John Larco, John Bieber
+ * CLASS: Grid.java
+ * PURPOSE: A dynamically sized grid for the PuzzlePiece class
+ */
 public class Grid {
 	
+	/*
+	 * PURPOSE: Constructor taking the width and height of the grid respectively
+	 * RETURNS: <object Grid>
+	 */
 	public Grid(int width, int height){
 		this.grid = new PuzzlePiece[height][width];
 	}
 	
+	/*
+	 * PURPOSE: Returns true if a specified location is within the grid, false if not.
+	 * RETURNS: boolean
+	 */
 	public boolean isValid(int x, int y){
 		return x > -1 && x < getWidth() && y > -1 && y < getHeight();
 	}
 	
+	/*
+	 * PURPOSE: Sets a location in the grid, specified by x and y, setting any provided PuzzlePiece there.
+	 * RETURNS: <object PuzzlePiece> The old puzzle piece in that location or null if there was none.
+	 */
 	public PuzzlePiece setCell(int x, int y, PuzzlePiece piece){
 		if(!isValid(x,y)) return null;
 		PuzzlePiece tempPiece = getCell(x,y);
@@ -17,32 +33,60 @@ public class Grid {
 		return tempPiece;
 	}
 	
+	/*
+	 * PURPOSE: Returns the PuzzlePiece at the specified (x, y) location. Null if there is none.
+	 * RETURNS: <object PuzzlePiece>
+	 */
 	public PuzzlePiece getCell(int x, int y){
 		return isValid(x,y) ? grid[y][x] : null;
 	}
 	
+	/*
+	 * PURPOSE: Getter for the grid height
+	 * RETURNS: int
+	 */
 	public int getHeight(){
 		return grid.length;
 	}
 	
+	/*
+	 * PURPOSE: Getter for the grid width
+	 * RETURNS: int
+	 */
 	public int getWidth(){
 		return grid[0].length;
 	}
 	
+	/*
+	 * PURPOSE: Returns true if the specified (x, y) location contains a PuzzlePiece object. False if not,
+	 * 			or the location is invalid.
+	 * RETURNS: boolean
+	 */
 	public boolean isOccupied(int x, int y){
-		if(!isValid(x,y)) return false;
-		return grid[y][x] != null;
+		return getCell(x,y) != null;
 	}
 	
+	/*
+	 * PURPOSE: Returns true if every grid location contains a PuzzlePiece, false if not.
+	 * RETURNS: boolean
+	 */
 	public boolean isFull(){
 		return occupied == getHeight() * getWidth();
 	}
 	
+	/*
+	 * PURPOSE: Clears the grid
+	 * RETURNS: void
+	 */
 	public void clear(){
 		this.grid = new PuzzlePiece[getHeight()][getWidth()];
 		occupied = 0;
 	}
 	
+	/*
+	 * PURPOSE: Returns true if no pieces are in the grid, false if not.
+	 * RETURNS: boolean
+	 */
 	public boolean isEmpty(){
 		return occupied == 0;
 	}
@@ -51,7 +95,7 @@ public class Grid {
 		String output = "";
 		for(int row=-1; row<=getHeight(); row++){
 			for(int col=-1; col<=getWidth(); col++){
-				output += isValid(col, row) ? grid[row][col]==null?" ":"¥" : "¥";
+				output += isValid(col, row) ? grid[row][col]==null?" ":"Â¥" : "Â¥";
 				output += col!=getWidth()?" ":"";
 			}
 			output += row!=getHeight()?"\n":"";
@@ -59,6 +103,10 @@ public class Grid {
 		return output;
 	}
 	
+	/*
+	 * PURPOSE: Duplicates the piece
+	 * RETURNS: <object Grid>
+	 */
 	public Grid clone(){
 		Grid g = new Grid(getWidth(), getHeight());
 		for(int i=0; i<grid.length; i++)
@@ -67,55 +115,7 @@ public class Grid {
 		return g;
 	}
 	
-	public static void main(String[] args){
-		Grid g = new Grid(3,3);
-		System.out.println(g.isValid(0, 0));
-		System.out.println(g.isValid(-1, 0));
-		System.out.println(g.isValid(0, -1));
-		System.out.println(g.isValid(2, 2));
-		System.out.println(g.isValid(3, 2));
-		System.out.println(g.isValid(2, 3));
-		// isValid works
-		System.out.println(g);
-		PuzzlePiece p = new PuzzlePiece(1,2,-1,2);
-		System.out.println(g.setCell(1,0,p));
-		System.out.println(g);
-		System.out.println(g.getCell(1, 0)==p);
-		System.out.println(g.setCell(1,0,new PuzzlePiece(1,2,-1,2))==p);
-		System.out.println(g.getCell(1, 0)==p);
-		System.out.println(g);
-		g.setCell(1,0,null);
-		System.out.println(g);
-		// getCell and setCell work
-		System.out.println(g.isEmpty());
-		System.out.println(g.setCell(1,0,p));
-		System.out.println(g.setCell(1,1,p));
-		System.out.println(g.setCell(2,2,p));
-		System.out.println(g);
-		g.clear();
-		System.out.println(g);
-		System.out.println(g.isEmpty());
-		for(int x=0; x<g.getWidth(); x++)
-			for(int y=0; y<g.getHeight(); y++){
-				g.setCell(x,y,p);
-				System.out.println(g.isFull());
-			}
-		// clear, isEmpty, getHeight, getWidth, and isFull work
-		System.out.println(g.isOccupied(1,2));
-		g.setCell(1,2,null);
-		System.out.println(g.isOccupied(1,2));
-		System.out.println(g.isFull());
-		// isOccupied works
-		System.out.println(g);
-		Grid g2 = g.clone();
-		g2.setCell(1,2,p);
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println(g);
-		System.out.println();
-		System.out.println(g2);
-	}
+	// ---------- PRIVATE DATA ---------- \\
 	
 	private PuzzlePiece[][] grid;
 	private int occupied;
